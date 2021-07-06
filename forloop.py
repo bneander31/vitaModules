b00161e1778ebe4fb129d17f194ff9cb3663f4c9
bneander31 <brent.neander@gmail.com>
2021-07-06 16:14:54 -0500

    Initial commit

---
+++ forloop.py
@@ -0,0 +1,12 @@
+friends = ["Jim", "Karen", "Kevin"]
+# for friend in friends:
+# for index in range(3, 10):
+# for index in range(len(friends)):
+for index in range(5):
+    # print(friend)
+    # print(index)
+    # print(friends[index])
+    if index == 0:
+        print("first iteration")
+    else:
+        print("Not first")
